b5d50a68259efbf65597ab9f3d02ab610e191f89
Matt Lambie <mlambie@lambie.org>
2022-10-17 20:56:53 +0800

    Recursive Inference (and the House of Windsor)

---
+++ windsor.pl
@@ -0,0 +1,16 @@
+parents_of(elizabeth-ii, geroge-vi, elizabeth).
+parents_of(margaret, george-vi, elizabeth).
+
+parents_of(charles, philip, elizabeth-ii).
+parents_of(anne, philip, elizabeth-ii).
+parents_of(andrew, philip, elizabeth-ii).
+parents_of(edward, philip, elizabeth-ii).
+
+parents_of(william, charles, diana).
+parents_of(harry, charles, diana).
+
+parents_of(george, william, kate).
+parents_of(charlotte, william, kate).
+parents_of(louis, william, kate).
+
+parents_of(archie, harry, meghan).
